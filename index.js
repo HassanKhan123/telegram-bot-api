60e0d7518022a1d5b36936eaeaa48a05374f2f94
HassanKhan123 <hk2602809@gmail.com>
2021-01-14 19:17:20 +0500

    more messages added

--- index.js
+++ index.js
@@ -12,16 +12,41 @@ const token = '1563119984:AAHNmLiXfP3nBxVhMT8Nb8M-xuHXiiaitSM';
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', (msg) => {
-  console.log('msg====', msg);
-  var hi = 'hi';
-  if (msg.text.toString().toLowerCase().indexOf(hi) === 0) {
+  var Hi = 'hi';
+  if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
     bot.sendMessage(msg.chat.id, 'Hello dear user');
   }
-
   var bye = 'bye';
   if (msg.text.toString().toLowerCase().includes(bye)) {
     bot.sendMessage(msg.chat.id, 'Hope to see you around again , Bye');
   }
+  var robot = "I'm robot";
+  if (msg.text.indexOf(robot) === 0) {
+    bot.sendMessage(msg.chat.id, "Yes I'm robot but not in that way!");
+  }
+  var Hi = 'hi';
+  if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+    bot.sendMessage(msg.from.id, 'Hello  ' + msg.from.first_name);
+  }
+  var location = 'location';
+  if (msg.text.indexOf(location) === 0) {
+    bot.sendLocation(msg.chat.id, 44.97108, -104.27719);
+    bot.sendMessage(msg.chat.id, 'Here is the point');
+  }
+});
+
+bot.onText(/\/sendpic/, (msg) => {
+  bot.sendPhoto(msg.chat.id, 'https://www.somesite.com/image.jpg', {
+    caption: 'Here we go ! \nThis is just a caption ',
+  });
+});
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'Welcome', {
+    reply_markup: {
+      keyboard: [['Sample text', 'Second sample'], ['Keyboard'], ["I'm robot"]],
+    },
+  });
 });
 bot.on('polling_error', (error) => {
   console.log(error.code); // => 'EFATAL'
